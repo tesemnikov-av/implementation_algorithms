0a78b89c9e99009a698f497b3542e417451956a0
tesemnikov-av <54743742+tesemnikov-av@users.noreply.github.com>
2020-12-24 16:27:12 +0300

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -8,43 +8,6 @@ class LinearRegression:
     def __init__(self):
       pass
 
-    def plot(self, xlabel='xlabel', ylabel='ylabel', title='title'):
-      plt.figure(figsize=(20,10))
-      scores = round(np.mean(self.cv(self.X, self.y)))
-      colours = ["#348ABD", "#A60628"]
-      plot_x = [min(self.X), max(self.X)]
-      plot_y = [self.coef_[0] * x + self.coef_[1] for x in plot_x ]
-      plt.scatter(self.X, self.y, label='Data', color=colours[0])
-      plt.plot(plot_x, plot_y, label='Model, RMSE: ' + str(scores), color=colours[1])
-      plt.legend()
-      plt.xlabel(xlabel)
-      plt.ylabel(ylabel)
-      plt.title('Linear Regression {} * x + {} '.format(self.k, self.b))
-      plt.grid()
-      plt.show()
-
-    def coef_(self):
-        return self.k[0], self.b[0]
-
-    def predict(self, x):
-        list = [ self.coef_[0] * x + self.coef_[1] for x in x ]
-        return list
-
-    def rmse(self, predictions, targets):
-        return np.sqrt((np.mean((predictions-targets)**2)))
-
-    def cv(self, X, y, cv=2):
-        batch = len(X)/cv
-        start = 0
-        score = []
-        for i in range(cv):
-            end = int((i+1)*batch)
-            predictions = self.predict(X[start:end])
-            targets = y[start:end]
-            start = int(start + batch)
-            score.append(self.rmse(predictions, targets)) 
-        return score
-
     def fit(self, X, y):
         if len(X) != len(y):
             raise ValueError('X ({}) and y ({}) must have the same length'.format(len(X), len(y)))
@@ -60,3 +23,47 @@ class LinearRegression:
         self.b = round((np.mean(y) - self.k * np.mean(X)))
       
         self.coef_ = [self.k, self.b]
+
+    def coef_(self):
+        return self.k[0], self.b[0]
+
+    def predict(self, x):
+        preds = [ self.coef_[0] * x + self.coef_[1] for x in x ]
+        return preds
+
+    def rmse(self, predictions, targets):
+        return np.sqrt((np.mean((predictions-targets)**2)))
+
+    def cv(self, X, y, cv=2):
+        """
+            Cross-validation,sometimes called rotation estimation or out-of-sample testing, 
+        """
+        batch = len(X)/cv
+        start = 0
+        score = []
+        for i in range(cv):
+            end = int((i+1)*batch)
+            predictions = self.predict(X[start:end])
+            targets = y[start:end]
+            start = int(start + batch)
+            score.append(self.rmse(predictions, targets)) 
+        return score
+        
+    def plot(self, xlabel='xlabel', ylabel='ylabel', title='title'):
+        """
+            Data and models visualization
+        """
+        plt.figure(figsize=(20,10))
+        scores = round(np.mean(self.cv(self.X, self.y)))
+        colours = ["#348ABD", "#A60628"]
+        plot_x = [min(self.X), max(self.X)]
+        plot_y = [self.coef_[0] * x + self.coef_[1] for x in plot_x ]
+        plt.scatter(self.X, self.y, label='Data', color=colours[0])
+        plt.plot(plot_x, plot_y, label='Model, RMSE: ' + str(scores), color=colours[1])
+        plt.legend()
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.title('Linear Regression {} * x + {} '.format(self.k, self.b))
+        plt.grid()
+        plt.show()
+        
